b979e58e08925e483defeaa5383ad0088ca86fb7
south-coast-coder <charliejarman1989@yahoo.com>
2024-08-29 17:32:31 +0100

    Second

--- react_notes/src/App.js
+++ react_notes/src/App.js
@@ -9,15 +9,8 @@ function App() {
         <p>
           Notes coming soon ...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+     
     </div>
   );
 }
